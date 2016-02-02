58aab84d7a9295dfd4c8a641bf27712acc318260
MisterOzzy <ozzy2106@gmail.com>
2016-02-02 14:43:11 +0200

    Changed IUnitOfWork(added properties for repos), Added project for business logic

--- Scambio/Scambio.DataAccess/Infrastructure/IUnitOfWork.cs
+++ Scambio/Scambio.DataAccess/Infrastructure/IUnitOfWork.cs
@@ -1,7 +1,15 @@
-﻿namespace Scambio.DataAccess.Infrastructure
+﻿using System.Security.Cryptography.X509Certificates;
+using Scambio.DataAccess.Repositories;
+
+namespace Scambio.DataAccess.Infrastructure
 {
     public interface IUnitOfWork
     {
+        IExternalLoginRepository ExternalLoginRepository { get; }
+        ILikeRepository LikeRepository { get; }
+        IPictureRepository PictureRepository { get; }
+        IPostRepository PostRepository { get; }
+        IUserRepository UserRepository { get; }
         void Save();
     }
 }
---
+++ Scambio/Scambio.Logic/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scambio.Logic
+{
+    public class Class1
+    {
+    }
+}
